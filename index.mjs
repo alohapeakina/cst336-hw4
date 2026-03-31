359b80e2a61718f51118125aefb0f281aa33ea19
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2026-03-30 21:24:03 -0700

    Added connection to trello API

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
 app.set("view engine", "ejs");
@@ -10,10 +12,30 @@ app.get('/', (req, res) => {
    res.render("index", {page_name});
 });
 
-app.get('/kanban', (req, res) => {
+app.get('/kanban', async (req, res) => {
    console.log("Loading kanban page");
    let page_name = "kanban";
-   res.render("kanban", {page_name});
+
+   const apiKey = process.env.TRELLO_API_KEY;
+   const token = process.env.TRELLO_API_TOKEN;
+   const boardId = process.env.TRELLO_BOARD_ID;
+
+   const url = `https://api.trello.com/1/boards/${boardId}?lists=open&cards=open&key=${apiKey}&token=${token}`;
+
+   try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      res.render("kanban", {
+         page_name,
+         lists: data.lists,
+         cards: data.cards
+      });
+   } catch (error) {
+      console.error("Failed to fetch Trello data: ", error);
+      res.render("kanban", {page_name, lists: [], cards: [] });
+   }
+
 });
 
 app.get('/lean', (req, res) => {
